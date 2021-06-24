3f49a9a9d457d276bb4a73b41602bad87f35829d
mikeyp957 <peraram2@gmail.com>
2021-06-23 20:01:27 -0700

    add projects and map onto project page

---
+++ app/src/pages/Portfolio.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Title from '../components/Title'
+import ProjectCard from '../components/ProjectCard'
+import ProjectsArr from '../projects/projects.json'
+
+function Portfolio() {
+return (
+    <div>
+       <Title title={'Porfolio'} />
+        <div>
+            {ProjectsArr.map((proj, index) => (
+                <ProjectCard
+                key={index}
+                title ={proj.title}
+                url ={proj.url}
+                image={proj.image}
+                description ={proj.description}
+                />
+            ))}
+        </div>
+    </div>
+)
+}
+
+export default Portfolio
