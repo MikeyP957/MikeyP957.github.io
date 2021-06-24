d6afc4285b82e923158e5473e8841c39cf4ea5a4
mikeyp957 <peraram2@gmail.com>
2021-06-23 18:01:17 -0700

    navbar component created

--- app/src/components/Nav/index.js
+++ app/src/components/Nav/index.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
+import { MenuItems } from './MenuItems'
+ import './Navbar.css'
+import { Link } from 'react-router-dom'
+import 'react-bootstrap'
+import { NavItem } from "react-bootstrap";
 
-function Nav() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-       link goes here
-      </a>
-    </nav>
-  );
+class Navbar extends Component {
+  state= { clicked: false }
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked })
+  }
+
+  render() {
+    return (
+      <nav className='NavbarItems'>
+        <h1 className= 'navbar-logo' >
+          REACT <i className= 'fab fa-react'></i> 
+        </h1>
+        <div className = 'menu-icon' onClick ={this.handleClick}>
+          <i className ={this.state.clicked ? 'fas fa-times' : 'fas fa-bars' }> </i>
+        </div>
+        <ul className = {this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+          {MenuItems.map((item, index) => {
+            return(
+              <li key={index}> 
+                <a className= {item.cName} href = {item.url}>
+                  {item.title}
+                </a> 
+              </li>
+            )
+          })}          
+        </ul>
+      </nav>
+    )
+  }
 }
 
-export default Nav;
+export default Navbar;
